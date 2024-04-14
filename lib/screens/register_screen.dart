79646669c9356b1b3319236adc1ad02744566dcb
zikoo <rikott98@gmail.com>
2024-04-14 14:06:58 +0200

    use a firebase and make an authentication and make methods help to do that

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 import '../widgets/custom_matrial_button.dart';
 import '../widgets/custom_text_form_field.dart';
@@ -12,85 +14,118 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  TextEditingController emailController = new TextEditingController();
+TextEditingController passwordController = new TextEditingController();
+bool isVisabile = false;
+var formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    TextEditingController emailController = new TextEditingController();
-    TextEditingController passwordController = new TextEditingController();
-    bool isVisabile = false;
+
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Column(
-              children: [
-                Image.asset(
-                  "assets/images/chatlogo.png",
-                  fit: BoxFit.cover,
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-                CustomTextFormField(
-                  label: "First Name",
-                  title: "enter your first name",
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                CustomTextFormField(
-                  label: "Last Name",
-                  title: "enter your first name",
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                CustomTextFormField(
-                  validator: (value) {
-                    if (value == null || value.trim().isEmpty) {
-                      return "Please enter your e-mail address";
-                    }
-                    var regex = RegExp(
-                        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-                    if (!regex.hasMatch(value)) {
-                      return "invalid e-mail address";
-                    }
-                  },
-                  controller: emailController,
-                  label: "Email",
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                CustomTextFormField(
-                  obsecure: !isVisabile,
-                  validator: (value) {
-                    if (value == null || value.trim().isEmpty) {
-                      return "Please enter your password";
-                    }
-                    return null;
-                  },
-                  suffixIcon: GestureDetector(
-                    onTap: () {
-                      isVisabile = !isVisabile;
-                      setState(() {});
-                    },
-                    child: isVisabile == true
-                        ? Icon(Icons.visibility_off_outlined)
-                        : Icon(Icons.visibility),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Column(
+                children: [
+                  Image.asset(
+                    "assets/images/chatlogo.png",
+                    fit: BoxFit.cover,
                   ),
-                  controller: passwordController,
-                  label: "password",
-                ),
-                CustomMatrialButton(
-                    color: Colors.blue, text: "Register", onClick: () {})
-              ],
-            )
-          ],
+                  SizedBox(
+                    height: 40,
+                  ),
+                  CustomTextFormField(
+                    label: "First Name",
+                    title: "enter your first name",
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  CustomTextFormField(
+                    label: "Last Name",
+                    title: "enter your first name",
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  CustomTextFormField(
+                    validator: (value) {
+                      if (value == null || value.trim().isEmpty) {
+                        return "Please enter your e-mail address";
+                      }
+                      var regex = RegExp(
+                          r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+                      if (!regex.hasMatch(value)) {
+                        return "invalid e-mail address";
+                      }
+                    },
+                    controller: emailController,
+                    label: "Email",
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  CustomTextFormField(
+                    obsecure: !isVisabile,
+                    validator: (value) {
+                      if (value == null || value.trim().isEmpty) {
+                        return "Please enter your password";
+                      }
+                      return null;
+                    },
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        isVisabile = !isVisabile;
+                        setState(() {});
+                      },
+                      child: isVisabile == true
+                          ? Icon(Icons.visibility_off_outlined)
+                          : Icon(Icons.visibility),
+                    ),
+                    controller: passwordController,
+                    label: "password",
+                  ),
+                  CustomMatrialButton(
+                      color: Colors.blue, text: "Register", onClick: () async {
+                        await register(context);
+                      },
+
+                      )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+  register(BuildContext context) async {
+    if (formKey.currentState!.validate()) {
+      EasyLoading.show();
+      try {
+        //call api
+        final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+            email: emailController.text, password: passwordController.text);
+        EasyLoading.dismiss();
+        Navigator.pop(context);
+        print(credential.user?.uid);
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'weak-password') {
+          EasyLoading.dismiss();
+          print('The password provided is too weak.');
+        } else if (e.code == 'email-already-in-use') {
+          print('The account already exists for that email.');
+          EasyLoading.dismiss();
+        }
+      } catch (e) {
+        print(e);
+      }
+    }
   }
 }
