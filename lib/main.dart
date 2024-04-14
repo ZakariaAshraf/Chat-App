8699360b42e610cf9f299a148c012914a80a0112
zikoo <rikott98@gmail.com>
2024-04-14 14:06:58 +0200

    feat: Implement Firebase Authentication
    **This commit adds user authentication functionality to the Flutter app using Firebase.
    **It includes email/password authentication and Google Sign-In.
    **The app now supports login, registration.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:chatapp/firebase_options.dart';
 import 'package:chatapp/screens/chat_screen.dart';
 import 'package:chatapp/screens/register_screen.dart';
 import 'package:chatapp/screens/sign_in_screen.dart';
 import 'package:chatapp/screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -17,13 +22,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Chat App',
-      initialRoute: WelcomScreen.RouteName,
+      initialRoute: ChatScreen.RouteName,
       routes: {
           WelcomScreen.RouteName:(context) => WelcomScreen(),
         SignIn.RouteName:(context) =>SignIn(),
         Register.RouteName:(context) =>Register(),
         ChatScreen.RouteName:(context) =>ChatScreen(),
     },
+      builder: EasyLoading.init(),
     );
   }
 }
--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,6 +1,10 @@
+import 'package:chatapp/screens/chat_screen.dart';
 import 'package:chatapp/widgets/custom_matrial_button.dart';
 import 'package:chatapp/widgets/custom_text_form_field.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+
 
 class SignIn extends StatefulWidget {
   SignIn({Key? key}) : super(key: key);
@@ -14,58 +18,83 @@ class _SignInState extends State<SignIn> {
   TextEditingController emailController = new TextEditingController();
   TextEditingController passwordController = new TextEditingController();
   bool isVisabile = false;
+  var formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Column(
-            children: [
-              Image.asset(
-                "assets/images/chatlogo.png",
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 40,),
-              CustomTextFormField(validator: (value) {
-                if (value == null || value.trim().isEmpty) {
-                  return "Please enter your e-mail address";
-                }
-                var regex = RegExp(
-                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-                if (!regex.hasMatch(value)) {
-                  return "invalid e-mail address";
-                }
-              },
-                controller: emailController,
-                label: "Email",),
-              SizedBox(height: 20,),
-              CustomTextFormField( obsecure: !isVisabile,
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return "Please enter your password";
-                  }
-                  return null;
-                },
-                suffixIcon: GestureDetector(
-                  onTap: () {
-                    isVisabile = !isVisabile;
-                    setState(() {});
-                  },
-                  child: isVisabile == true
-                      ? Icon(Icons.visibility_off_outlined)
-                      : Icon(Icons.visibility),
+      body: Form(
+        key: formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Column(
+              children: [
+                Image.asset(
+                  "assets/images/chatlogo.png",
+                  fit: BoxFit.cover,
                 ),
-                controller: passwordController,
-                label: "password",),
-              CustomMatrialButton(color: Colors.blue, text: "Let's Go", onClick: (){})
-            ],
-          )
-        ],
+                SizedBox(height: 40,),
+                CustomTextFormField(validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return "Please enter your e-mail address";
+                  }
+                  var regex = RegExp(
+                      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+                  if (!regex.hasMatch(value)) {
+                    return "invalid e-mail address";
+                  }
+                },
+                  controller: emailController,
+                  label: "Email",),
+                SizedBox(height: 20,),
+                CustomTextFormField( obsecure: !isVisabile,
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return "Please enter your password";
+                    }
+                    return null;
+                  },
+                  suffixIcon: GestureDetector(
+                    onTap: () {
+                      isVisabile = !isVisabile;
+                      setState(() {});
+                    },
+                    child: isVisabile == true
+                        ? Icon(Icons.visibility_off_outlined)
+                        : Icon(Icons.visibility),
+                  ),
+                  controller: passwordController,
+                  label: "password",),
+                CustomMatrialButton(color: Colors.blue, text: "Let's Go", onClick: () => login(),)
+              ],
+            )
+          ],
 
+        ),
       ),
     );
+  }
+  login() async {
+    if (formKey.currentState!.validate()) {
+      EasyLoading.show();
+      try {
+        //call api
+        final credential = await FirebaseAuth.instance
+            .signInWithEmailAndPassword(
+            email: emailController.text, password: passwordController.text);
+        EasyLoading.dismiss();
+        Navigator.pushNamedAndRemoveUntil(context, ChatScreen.RouteName, (route) => false);
+        print(credential.user?.uid);
+
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'user-not-found') {
+          print('No user found for that email.');
+        } else if (e.code == 'wrong-password') {
+          print('Wrong password provided for that user.');
+        }
+      }
+    }
   }
 }
